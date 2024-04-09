6bb461cda30ab382f6fd8934de37f6a3e52ae81d
tokarczykbl <tokarczykbl@users.noreply.github.com>
2024-04-09 17:06:46 -0400

    Saving testing area

--- static/js/genre_table_insert.js
+++ static/js/genre_table_insert.js
@@ -8,12 +8,12 @@ table.classed("table-striped", true);
 // Use D3 to select the table body
 let tbody = d3.select("tbody");
 
-url = "http://127.0.0.1:5000/api/v1.0/genres";
+genre_url = "http://127.0.0.1:5000/api/v1.0/genres";
 
 // BONUS: Dynamic table
 // Loop through an array of grades and build the entire table body from scratch
 // Fetch data from the Flask API
-fetch(url)
+fetch(genre_url)
   .then(response => response.json())
   .then(data => {
     // Loop through the data and populate the table
@@ -26,3 +26,30 @@ fetch(url)
     });
   })
   .catch(error => console.error('Error fetching data:', error));
+
+
+names_url = "http://127.0.0.1:5000/api/v1.0/attractions_unique_names"
+
+fetch(names_url)
+  .then(response => response.json())
+  .then(data => {
+    let dropdown = document.getElementById("selDataset");
+
+    data.forEach(attraction => {
+      let option = document.createElement("option");
+      option.value = attraction;
+      option.textContent = attraction;
+      dropdown.appendChild(option);
+    });
+  })
+  .catch(error => console.error('Error fetching data:', error));
+
+
+  let myMap = L.map("map2").setView([39.8283, -98.5795], 5);
+
+  // Add a tile layer (the background map image) to our map.
+  // Use the addTo() method to add objects to our map.
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+
